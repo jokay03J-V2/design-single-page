71d1001b8db82ad0244393f2d5d8688bf78d82d4
jokay03J-V2 <jmagro@edenschool.fr>
2023-01-09 17:11:20 +0100

    add carrousel

--- main.js
+++ main.js
@@ -4,36 +4,38 @@ let carrouselIndex = 1;
 let carrouselTransform = -200;
 const carrouselContainer = document.getElementsByClassName("carrousel__container")
 const carrouselImgs = document.querySelectorAll (".carrousel__item")
-const carrouselArrowLeft = document
-.querySelector("#carrousel__arrow_left");
-const carrouselArrowRight = document
-.querySelector("#carrousel__arrow_right");
-const slideWidth = carrouselImgs[0].clientWidth;
-carrouselContainer[0].scrollLeft += slideWidth;
+const Imgs = ["./assets/image-slide-1.jpg", "./assets/image-slide-2.jpg", "./assets/image-slide-3.jpg", "./assets/image-slide-4.jpg", "./assets/image-slide-5.jpg"];
+const firstImg = document.getElementById("carrousel__item1")
+const secondeImg = document.getElementById("carrousel__item2")
+const threeImg = document.getElementById("carrousel__item3")
 
 // carrousel controls
 document
   .querySelector("#carrousel__arrow_right")
   .addEventListener("click", (ev) => {
-    if (carrouselIndex === carrouselImgs.length - 1) {
+    if (carrouselIndex === Imgs.length - 1) {
       carrouselIndex = 0;
-      carrouselTransform = -200;
     } else {
-      carrouselTransform += 100;
+      carrouselIndex++
+      for (let index = 0; index < carrouselImgs.length; index++) {
+        const img = carrouselImgs[index];
+        img.classList.remove("carrousel__item_active")
+      }
+      carrouselImgs[carrouselIndex].classList.add("carrousel__item_active")
     }
-    const slideWidth = carrouselImgs[0].clientWidth;
-    carrouselContainer[0].scrollLeft -= slideWidth;
 });
 
 document
   .querySelector("#carrousel__arrow_left")
   .addEventListener("click", (ev) => {
     if (carrouselIndex === 0) {
-      carrouselIndex = carrouselImgs.length - 1;
-      carrouselTransform = -200;
+      carrouselIndex = Imgs.length - 1;
     } else {
-      carrouselIndex-= 100;
+      carrouselIndex--
+      for (let index = 0; index < carrouselImgs.length; index++) {
+        const img = carrouselImgs[index];
+        img.classList.remove("carrousel__item_active")
+      }
+      carrouselImgs[carrouselIndex].classList.add("carrousel__item_active")
     }
-    const slideWidth = carrouselImgs[0].clientWidth;
-    carrouselContainer[0].scrollLeft += slideWidth;
 });
