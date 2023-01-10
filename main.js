dff5be79ddc85e4bf9a008cb9a1f493da65ad674
jokay03J-V2 <jmagro@edenschool.fr>
2023-01-10 11:54:24 +0100

    fix wrong carousel width and carrousel => carousel

--- main.js
+++ main.js
@@ -1,41 +1,41 @@
 import "./style.css";
 
-let carrouselIndex = 1;
-let carrouselTransform = -200;
-const carrouselContainer = document.getElementsByClassName("carrousel__container")
-const carrouselImgs = document.querySelectorAll (".carrousel__item")
+let carouselIndex = 1;
+let carouselTransform = -200;
+const carouselContainer = document.getElementsByClassName("carousel__container")
+const carouselImgs = document.querySelectorAll (".carousel__item")
 const Imgs = ["./assets/image-slide-1.jpg", "./assets/image-slide-2.jpg", "./assets/image-slide-3.jpg", "./assets/image-slide-4.jpg", "./assets/image-slide-5.jpg"];
-const firstImg = document.getElementById("carrousel__item1")
-const secondeImg = document.getElementById("carrousel__item2")
-const threeImg = document.getElementById("carrousel__item3")
+const firstImg = document.getElementById("carousel__item1")
+const secondeImg = document.getElementById("carousel__item2")
+const threeImg = document.getElementById("carousel__item3")
 
-// carrousel controls
+// carousel controls
 document
-  .querySelector("#carrousel__arrow_right")
+  .querySelector("#carousel__arrow_right")
   .addEventListener("click", (ev) => {
-    if (carrouselIndex === Imgs.length - 1) {
-      carrouselIndex = 0;
+    if (carouselIndex === Imgs.length - 1) {
+      carouselIndex = 0;
     } else {
-      carrouselIndex++
-      for (let index = 0; index < carrouselImgs.length; index++) {
-        const img = carrouselImgs[index];
-        img.classList.remove("carrousel__item_active")
+      carouselIndex++
+      for (let index = 0; index < carouselImgs.length; index++) {
+        const img = carouselImgs[index];
+        img.classList.remove("carousel__item_active")
       }
-      carrouselImgs[carrouselIndex].classList.add("carrousel__item_active")
+      carouselImgs[carouselIndex].classList.add("carousel__item_active")
     }
 });
 
 document
-  .querySelector("#carrousel__arrow_left")
+  .querySelector("#carousel__arrow_left")
   .addEventListener("click", (ev) => {
-    if (carrouselIndex === 0) {
-      carrouselIndex = Imgs.length - 1;
+    if (carouselIndex === 0) {
+      carouselIndex = Imgs.length - 1;
     } else {
-      carrouselIndex--
-      for (let index = 0; index < carrouselImgs.length; index++) {
-        const img = carrouselImgs[index];
-        img.classList.remove("carrousel__item_active")
+      carouselIndex--
+      for (let index = 0; index < carouselImgs.length; index++) {
+        const img = carouselImgs[index];
+        img.classList.remove("carousel__item_active")
       }
-      carrouselImgs[carrouselIndex].classList.add("carrousel__item_active")
+      carouselImgs[carouselIndex].classList.add("carousel__item_active")
     }
 });
