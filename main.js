248862d5576b6c07c15eb7a6634adbe4c3bafee6
jokay03J-V2 <jmagro@edenschool.fr>
2023-04-13 08:55:05 +0200

    add sasslib and update single page design portfolio with sasslib

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
+import "@jokay03j-v2/sasslib";
 import "./style.css";
+// let carouselIndex = 2;
+// const carouselImgs = document.querySelectorAll(".carousel__item");
+// const carouselImgsContainer =
+//   document.getElementsByClassName("carousel__imgs")[0];
+// let carouselTransforms = (carouselImgs[0].clientWidth + 15) * -2;
 
-let carouselIndex = 2;
-const carouselImgs = document.querySelectorAll(".carousel__item");
-const carouselImgsContainer =
-  document.getElementsByClassName("carousel__imgs")[0];
-let carouselTransforms = (carouselImgs[0].clientWidth + 15) * -2;
+// carouselImgsContainer.style.transform = `translateX(${carouselTransforms}px)`;
 
-carouselImgsContainer.style.transform = `translateX(${carouselTransforms}px)`;
+// // carousel controls
+// document
+//   .querySelector("#carousel__arrow_left")
+//   .addEventListener("click", (ev) => {
+//     if (carouselIndex === carouselImgs.length - 1) {
+//       carouselIndex = 0;
+//       carouselTransforms = 0;
+//     } else {
+//       carouselIndex++;
+//       carouselTransforms -= carouselImgs[0].clientWidth + 15;
+//     }
+//     carouselImgsContainer.style.transform = `translateX(${carouselTransforms}px)`;
+//   });
 
-// carousel controls
-document
-  .querySelector("#carousel__arrow_left")
-  .addEventListener("click", (ev) => {
-    if (carouselIndex === carouselImgs.length - 1) {
-      carouselIndex = 0;
-      carouselTransforms = 0;
-    } else {
-      carouselIndex++;
-      carouselTransforms -= carouselImgs[0].clientWidth + 15;
-    }
-    carouselImgsContainer.style.transform = `translateX(${carouselTransforms}px)`;
-  });
-
-document
-  .querySelector("#carousel__arrow_right")
-  .addEventListener("click", (ev) => {
-    if (carouselIndex === 0) {
-      carouselIndex = carouselImgs.length - 1;
-      carouselTransforms = -(carouselImgs[0].clientWidth + 15) * carouselIndex;
-    } else {
-      carouselIndex--;
-      carouselTransforms += carouselImgs[0].clientWidth + 15;
-    }
-    carouselImgsContainer.style.transform = `translateX(${carouselTransforms}px)`;
-  });
+// document
+//   .querySelector("#carousel__arrow_right")
+//   .addEventListener("click", (ev) => {
+//     if (carouselIndex === 0) {
+//       carouselIndex = carouselImgs.length - 1;
+//       carouselTransforms = -(carouselImgs[0].clientWidth + 15) * carouselIndex;
+//     } else {
+//       carouselIndex--;
+//       carouselTransforms += carouselImgs[0].clientWidth + 15;
+//     }
+//     carouselImgsContainer.style.transform = `translateX(${carouselTransforms}px)`;
+//   });
